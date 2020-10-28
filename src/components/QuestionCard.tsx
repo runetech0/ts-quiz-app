34f2e687e6f8b044834f79788353ae6a53d31971
Rehman Ali <rehmanali.9442289@gmail.com>
2020-10-28 19:48:53 +0500

    Remove additional imports

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react'
 import { Button } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
